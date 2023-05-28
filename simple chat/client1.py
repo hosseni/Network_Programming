685c2415ce19326f1f9a31a7e07281f7a1a3c03a
unknown <elhossenenour@gmail.com>
2023-05-29 01:38:59 +0300

    Simple Chat

---
+++ simple chat/client1.py
@@ -0,0 +1,16 @@
+from socket import *
+try :
+    s = socket (AF_INET, SOCK_STREAM)
+    host = "127.0.0.1"
+    port = 7002
+    s.connect ((host, port))
+    while True:
+        send_data = input ("client : ")
+        s.send (send_data.encode('utf-8'))
+        recv_data = s.recv(2048)
+        print("Server : ", recv_data.decode ('utf-8'))
+    s.close()
+
+
+except KeyboardInterrupt :
+    print("chat is terminated")
